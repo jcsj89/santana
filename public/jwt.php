4831c142a92d6303d447418ff4738990d1206eeb
= <jcsj2010@gmail.com>
2021-02-01 16:21:43 -0300

    01.02.21 pg produtos

---
+++ public/jwt.php
@@ -0,0 +1,20 @@
+<?php
+$header = [
+   'alg' => 'HS256',
+   'typ' => 'JWT'
+];
+$header = json_encode($header);
+$header = base64_encode($header);
+
+$payload = [
+   'iss' => 'localhost',
+   'name' => 'Jose',
+   'email' => 'jcsj2010@gmail.com'
+];
+$payload = json_encode($payload);
+$payload = base64_encode($payload);
+
+$signature = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
+$signature = base64_encode($signature);
+
+echo "$header.$payload.$signature";
